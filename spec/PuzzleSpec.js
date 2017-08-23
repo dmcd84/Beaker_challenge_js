bbd44c8de8c974f8705c78918b3802055da59d92
Dom <dom.mcdonnell@gmail.com>
2017-08-23 09:31:24 +0100

    Can pour from one beaker to another

--- spec/PuzzleSpec.js
+++ spec/PuzzleSpec.js
@@ -4,7 +4,7 @@ describe('Puzzle', function() {
   var puzzle;
 
   beforeEach(function(){
-    puzzle = new Puzzle(3,5);
+    puzzle = new Puzzle(3,5,4);
   })
 
   it('to require two beakers on initiation', function(){
@@ -12,4 +12,22 @@ describe('Puzzle', function() {
     expect(puzzle.beaker2).toBeDefined();
   })
 
+  it('will have a target volume on initiation', function(){
+    expect(puzzle.target).toBeDefined();
+  })
+
+  it('can pour from beaker one into beaker two', function(){
+    puzzle.beaker1.fillBeaker();
+    puzzle.pour(puzzle.beaker1, puzzle.beaker2)
+    expect(puzzle.beaker2.currentVolume).toEqual(3);
+    expect(puzzle.beaker1.currentVolume).toEqual(0);
+  })
+
+  it('can pour back into beaker two into beaker one', function(){
+    puzzle.beaker1.fillBeaker();
+    puzzle.pour(puzzle.beaker1, puzzle.beaker2)
+    puzzle.pour(puzzle.beaker2, puzzle.beaker1)
+    expect(puzzle.beaker2.currentVolume).toEqual(0);
+    expect(puzzle.beaker1.currentVolume).toEqual(3);
+  })
 });
--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,4 +1,16 @@
-function Puzzle(beaker1, beaker2){
+function Puzzle(beaker1, beaker2, target){
   this.beaker1 = new Beaker(beaker1);
   this.beaker2 = new Beaker(beaker2);
+  this.target = target;
+}
+
+Puzzle.prototype.pour = function(from, into){
+  var vol_spare = into.capacity - into.currentVolume;
+  if (from.currentVolume > vol_spare) {
+    from.currentVolume -= vol_spare
+    into.currentVolume += vol_spare
+  } else {
+    into.currentVolume += from.currentVolume
+    from.emptyBeaker();
+  }
 }
