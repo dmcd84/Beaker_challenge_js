17c064c24c739dd00aeb2ac14c6446b083f67089
Dom <dom.mcdonnell@gmail.com>
2017-11-14 16:05:58 +0000

    Dry out interface

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,51 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("#start-game").on('click', function() {
     console.log("New Game started");
-     let puzzle = new Puzzle($("#beaker1input").val(),$("#beaker2input").val(),$("#target-input").val());
-     $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-     $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
+    let puzzle = new Puzzle($("#beaker1input").val(), $("#beaker2input").val(), $("#target-input").val());
 
-     $("#fill-1").on('click', function() {
-       console.log("Filling beaker 1");
-       puzzle.beaker1.fillBeaker();
-       $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-       $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
-     });
+    $.fn.updateBeakerVolume = function() {
+      console.log('in update fn');
+      $("#beaker1volume").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
+      $("#beaker2volume").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
+    };
 
-     $("#fill-2").on('click', function() {
-       console.log("Filling beaker 2");
-       puzzle.beaker2.fillBeaker();
-       $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-       $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
-     });
+    $("#fill-1").on('click', function() {
+      console.log("Filling beaker 1");
+      puzzle.beaker1.fillBeaker();
+      $.fn.updateBeakerVolume();
+    });
 
-     $("#empty-1").on('click', function() {
-       console.log("Emptying beaker 1");
-       puzzle.beaker1.emptyBeaker();
-       $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-       $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
-     });
+    $("#fill-2").on('click', function() {
+      console.log("Filling beaker 2");
+      puzzle.beaker2.fillBeaker();
+      $.fn.updateBeakerVolume();
+    });
 
-     $("#empty-2").on('click', function() {
-       console.log("Emptying beaker 2");
-       puzzle.beaker2.emptyBeaker();
-       $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-       $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
-     });
+    $("#empty-1").on('click', function() {
+      console.log("Emptying beaker 1");
+      puzzle.beaker1.emptyBeaker();
+      $.fn.updateBeakerVolume();
+    });
 
-     $("#pour-into-1").on('click', function() {
-       console.log("Pouring beaker 2 into beaker 1");
-       puzzle.pour(puzzle.beaker2, puzzle.beaker1);
-       $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-       $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
-     });
+    $("#empty-2").on('click', function() {
+      console.log("Emptying beaker 2");
+      puzzle.beaker2.emptyBeaker();
+      $.fn.updateBeakerVolume();
+    });
 
-     $("#pour-into-2").on('click', function() {
-       console.log("Pouring beaker 1 into beaker 2");
-       puzzle.pour(puzzle.beaker1, puzzle.beaker2);
-       $("#beaker1capacity").text("Beaker 1 Current Volume:" + " " + puzzle.beaker1.currentVolume);
-       $("#beaker2capacity").text("Beaker 2 Current Volume:" + " " + puzzle.beaker2.currentVolume);
-     });
-   });
+    $("#pour-into-1").on('click', function() {
+      console.log("Pouring beaker 2 into beaker 1");
+      puzzle.pour(puzzle.beaker2, puzzle.beaker1);
+      $.fn.updateBeakerVolume();
+    });
 
+    $("#pour-into-2").on('click', function() {
+      console.log("Pouring beaker 1 into beaker 2");
+      puzzle.pour(puzzle.beaker1, puzzle.beaker2);
+      $.fn.updateBeakerVolume();
+    });
+  });
 })
